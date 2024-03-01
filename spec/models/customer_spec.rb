2ad6c3f50625979c19bf1081144e8b7212e51a87
vanderleipinto <vandecopinto@gmail.com>
2024-03-01 11:08:26 -0300

    feat: fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :customers #ou fixtures :all
+    
+    # subject.name = "Vanderlei Pinto"
+    # subject.email = "vandecopinto@gmail.com"
+    # subject.save
+
+  it 'Create a Customer' do 
+    customer = customers(:vanderlei)
+
+    expect(customer.name).to eq("Vanderlei Pinto")
+    expect(customer.full_name).to eq("Sr. Vanderlei Pinto")
+  end
 end
