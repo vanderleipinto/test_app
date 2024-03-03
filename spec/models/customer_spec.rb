13c714ceedcda38030a889452d35fa246247b17f
vanderleipinto <vandecopinto@gmail.com>
2024-03-03 15:50:44 -0300

    feat: FactoryBot (former FactoryGirl)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
 
-  fixtures :customers #ou fixtures :all
+# Creating using features
+#   fixtures :customers #ou fixtures :all
     
-    # subject.name = "Vanderlei Pinto"
-    # subject.email = "vandecopinto@gmail.com"
-    # subject.save
+#     # subject.name = "Vanderlei Pinto"
+#     # subject.email = "vandecopinto@gmail.com"
+#     # subject.save
+
+#   it 'Create a Customer' do 
+#     customer = customers(:vanderlei)
+
+#     expect(customer.name).to eq("Vanderlei Pinto")
+#     expect(customer.full_name).to eq("Sr. Vanderlei Pinto")
+#   end
+
+#  Creating using factory bot
 
   it 'Create a Customer' do 
-    customer = customers(:vanderlei)
+    customer = build (:customer)
+    expect(customer.full_name).to eq("Sr. Beatriz")
+  end    
 
-    expect(customer.name).to eq("Vanderlei Pinto")
-    expect(customer.full_name).to eq("Sr. Vanderlei Pinto")
-  end
 end
