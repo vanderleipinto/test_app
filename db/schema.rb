1a3815bb01fa7509559adb8e2643ae41688f285e
vanderleipinto <vandecopinto@gmail.com>
2024-03-04 07:48:17 -0300

    feat: Heranca

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_131925) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.boolean "vip"
+    t.integer "days_to_pay"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :customer, aliases: [:user, :worker] do
     name {Faker::Name.name}
     email {"beatriz@filha.com"}
+    
+    factory :customer_vip do
+      vip {true}
+      days_to_pay {30}
+    end
+
+    factory :customer_default do
+      vip {false}
+      days_to_pay {15}
+    end
   end
 end
