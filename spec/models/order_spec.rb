28b2175c343f521b2b7dc51d1860ecf064106c2a
vanderleipinto <vandecopinto@gmail.com>
2024-03-09 14:00:33 -0300

    feat: Create_list

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,9 +7,16 @@ RSpec.describe Order, type: :model do
   end
 
   it 'Verifica se o order possui customer' do
-    customer_manual = crete(:customer_female_vip)
+    customer_manual = create(:customer_female_vip)
     order = create(:order, customer: customer_manual) #sobrescrevendo o customer automático
     expect(order.customer).to be_kind_of(Customer)
   end
 
-end
+  it 'tem 3 pedidos' do
+    orders = create_list(:order, 3, description: "Testeee")
+    p orders.inspect
+    expect(orders.count).to eq(3)
+  end
+
+
+end 
