ef8b8ef230e0427c80a6420a283f68c9cb0ec580
vanderleipinto <vandecopinto@gmail.com>
2024-04-01 23:01:29 -0300

    feat: Webmock

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,9 +1,20 @@
 describe 'HTTParty' do
 
-  it 'HTTParty' do
+  it 'content-type' do
+    # stub_request(:get, "http://jsonplaceholder.typicode.com/posts/2").
+    # with(
+    #   headers: {
+    #  'Accept'=>'*/*',
+    #  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #  'User-Agent'=>'Ruby'
+    #   }).
+    # to_return(status: 200, body: "", headers: {'content-type':'application/json'})
+
+
+
+
     response = HTTParty.get('http://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers["content-type"]
-
     expect(content_type).to match(/application\/json/)
   end
 
